7217640ca12a6fa0308210b65b86211166b43780
Luiz Garcia <lacg@me.com>
2019-06-19 14:05:10 -0400

    Delete folders in the beginning of the process for clean up, before starts.

--- src/com/axway/gw/es/tools/ConvertToYamlStore.java
+++ src/com/axway/gw/es/tools/ConvertToYamlStore.java
@@ -2,6 +2,10 @@ package com.axway.gw.es.tools;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Comparator;
 import java.util.Properties;
 import java.util.logging.Logger;
 
@@ -24,8 +28,23 @@ public class ConvertToYamlStore {
         typeManager = new TypeManager(es);
         entityManager = new EntityManager(es);
       }
-	
-	
+
+	private void delete(String location)
+	{
+		File dir = new File(location);
+		if (dir.exists())
+			try {
+				Path rootPath = Paths.get(location);
+				Files.walk(rootPath)
+						.sorted(Comparator.reverseOrder())
+						.map(Path::toFile)
+						.peek(System.out::println)
+						.forEach(File::delete);
+			}
+			catch (IOException e)
+			{ LOGGER.info("Unable to delete directory " + location); }
+	}
+
 	public void dumpTypesAsYaml(String location) throws JsonGenerationException, JsonMappingException, IOException {
 		LOGGER.info("Dumping types to " + location);
 		typeManager.writeTypes(new File(location));
@@ -48,6 +67,9 @@ public class ConvertToYamlStore {
 		String whereToWriteEntities = args[1] + "/root";
 		try {
 			ConvertToYamlStore converter = new ConvertToYamlStore(ES_TO_LOAD, new Properties());
+			LOGGER.info("Deleting folders");
+			converter.delete(whereToWriteTypes);
+			converter.delete(whereToWriteEntities);
 			converter.dumpTypesAsYaml(whereToWriteTypes);
 			converter.dumpEntitesAsYaml(whereToWriteEntities);
 			LOGGER.info("Successfully extracted yaml files.");
