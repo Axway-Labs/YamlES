5ad6191b72a3e8c42bf2d1ddc2dd64a33b54a79b
wsalembi <willem.salembier@gmail.com>
2019-09-18 01:23:34 +0200

    Add cleanup routine based on template

--- src/test/java/com/axway/gw/es/tools/BatchYamlStoreConverter.java
+++ src/test/java/com/axway/gw/es/tools/BatchYamlStoreConverter.java
@@ -1,6 +1,13 @@
 package com.axway.gw.es.tools;
 
+import com.google.common.hash.HashCode;
+import com.google.common.hash.HashFunction;
+import com.google.common.hash.Hashing;
+
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 
 public class BatchYamlStoreConverter {
 
@@ -10,6 +17,41 @@ public class BatchYamlStoreConverter {
         convert("TeamDevelopmentAPI");
         convert("TeamDevelopmentSettings");
         convert("TeamDevelopmentSettingsAPIM");
+
+        clearSimilar("FactoryTemplate");
+        clearSimilar("FactoryTemplateSamples");
+        clearSimilar("TeamDevelopmentSettings");
+        clearSimilar("TeamDevelopmentSettingsAPIM");
+    }
+
+    private static void clearSimilar(String project) throws IOException {
+        Path reference = Paths.get("/tmp/yamlstores/TeamDevelopmentAPI");
+        Path base = Paths.get("/tmp/yamlstores/" + project);
+        Files.walkFileTree(base, new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                Path relativize = base.relativize(file);
+                Path original = reference.resolve(relativize);
+                if (!original.toFile().exists()) {
+                    return FileVisitResult.CONTINUE;
+                }
+                HashCode originalHash = com.google.common.io.Files.asByteSource(original.toFile()).hash(Hashing.goodFastHash(128));
+                HashCode newHash = com.google.common.io.Files.asByteSource(file.toFile()).hash(Hashing.goodFastHash(128));
+                if (newHash.equals(originalHash)) {
+                    file.toFile().delete();
+                }
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                File[] files = dir.toFile().listFiles();
+                if (files == null || files.length == 0) {
+                    dir.toFile().delete();
+                }
+                return FileVisitResult.CONTINUE;
+            }
+        });
     }
 
     private static void convert(String project) throws InterruptedException, IOException {
